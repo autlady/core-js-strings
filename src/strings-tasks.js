a1a75472b9fbea8afe5e6e7487bc86cc98ffe5e7
Julia Grigoreva <julia_panina@mail.ru>
2024-11-07 23:46:50 +0300

    feat: func concatenateStrings and getFirstChar

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -61,8 +61,9 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.concat(value2);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -76,8 +77,9 @@ function concatenateStrings(/* value1, value2 */) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
+  // throw new Error('Not implemented');
 }
 
 /**
